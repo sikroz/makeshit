6530693933f4585957bd25d7ed2b89acbdfee46e
Sergey Butakov <s.butakov@ispsystem.com>
2014-08-28 10:42:43 +0900

    Translate message and comments

--- makeshit.py
+++ makeshit.py
@@ -6,14 +6,15 @@ import string
 class Color:
     OK = '\033[92m'
     ERR = '\033[91m'
+    WARNING = '\e[0;35m'
     END = '\033[0m'
 
 class MakeShit:
     def __init__(self, path, count_files, total_size):
         """
-        :param path: Путь где надо нашитеть
-        :param count_files: Общее кол-во файлов
-        :param total_size: Общий размер всего shit (MB)
+        :param path: Path for make shit
+        :param count_files: Count of files
+        :param total_size: Size оf shit (MB)
         """
 
         self.path = path
@@ -35,34 +36,34 @@ class MakeShit:
             "/media/js", "/media/img", "/media/img/icons", "/media/img/sprites"
         ]
 
-        #Кол-во файлов в одной дире
+        #Count of files in directory
         cf = int(self.count_files / len(dir_list))
-        #Размер одного файла
+        #File size
         fs = int(self.total_size * 2**20 / self.count_files)
 
         for folder in dir_list:
             path = os.path.normpath(self.path + folder)
             try:
                 os.mkdir(path)
-                print(Color.OK + "[OK]" + Color.END, "Создание директории", path)
+                print(Color.OK + "[OK]" + Color.END, "Create directory", path)
             except FileNotFoundError:
-                print(Color.ERR + "[ERROR]" + Color.END, "Директория", path, "не существует")
+                print(Color.ERR + "[ERROR]" + Color.END, "Directory", path, "not exists")
             except PermissionError:
-                print(Color.ERR + "[ERROR]" + Color.END, "Недостаточно прав на создание", path, "директории")
+                print(Color.ERR + "[ERROR]" + Color.END, "Permission denied", path, "to create directory")
             except FileExistsError:
-                print(Color.ERR + "[ERROR]" + Color.END, "Директория", path, "уже существует")
+                print(Color.WARNING + "[WARNING]" + Color.END, "Directory", path, "exists")
 
             for i in range(cf):
                 file_name = os.path.join(path, self.generate_string(10) + "." + self.generate_string(3))
                 f = open(file_name, "w")
                 try:
                     f.write(self.generate_string(fs))
-                    print(Color.OK + "[OK]" + Color.END, "Создание файла", file_name)
+                    print(Color.OK + "[OK]" + Color.END, "Create file", file_name)
                 except FileNotFoundError:
-                    print(Color.ERR + "[ERROR]" + Color.END, "Файл", file_name, "не существует")
+                    print(Color.ERR + "[ERROR]" + Color.END, "File", file_name, "not exists")
                 except PermissionError:
-                    print(Color.ERR + "[ERROR]" + Color.END, "Недостаточно прав на создание файла", file_name)
+                    print(Color.ERR + "[ERROR]" + Color.END, "Permission denied to create file", file_name)
                 except FileExistsError:
-                    print(Color.ERR + "[ERROR]" + Color.END, "Файл", file_name, "уже существует")
+                    print(Color.WARNING + "[WARNING]" + Color.END, "File", file_name, "exists")
                 finally:
                     f.close()
